3444b83f57a8461d43f94a1bdee7f7e51e0670b0
TarekBenYahia <Tarekbenyahia89@gmail.com>
2021-10-23 09:48:13 +0100

    Sounds + Mic

---
+++ Assets/Scripts/CollisionSounds.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollisionSounds : MonoBehaviour
+{
+    private MeshRenderer m;
+    public AudioSource Hit;
+    public AudioSource PanelHit;
+    public AudioSource GroundHit;
+    public GameObject ballon;
+
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        m = this.GetComponent<MeshRenderer>();
+
+      
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "poteau")
+        {
+            Hit.Play();
+        }
+        if (collision.collider.tag == "panel")
+        {
+            PanelHit.Play();
+        }
+        if (collision.collider.tag == "ground")
+        {
+            GroundHit.Play();
+        }
+    }
+
+    
+}
