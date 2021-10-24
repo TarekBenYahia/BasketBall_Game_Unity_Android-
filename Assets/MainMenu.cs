bf6ba64d0ca1ab4548128f6e7b2d69f5d3db1a09
ferjani <houssem.ferjani@esprit.tn>
2021-10-24 10:51:19 +0100

    Menu Commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+ public void playgame()
+ {
+   SceneManager.LoadScene(1);
+
+ }
+ public void quitgame()
+ {
+     Application.Quit();  
+ }
+}
